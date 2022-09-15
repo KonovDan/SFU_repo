7922e3a90e97d0876cdd9002191a506ace3d711f
user <user@srv.faked>
2022-09-15 21:44:15 +0700

    15.09.2022

---
+++ PracWork_3/task_4/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите A"); double A = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите B"); double B = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите C"); double C = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите D"); double D = Convert.ToDouble(Console.ReadLine());
+
+double [] res = new double[10];
+double sump = 0, sum = 0;
+
+for (int x = 1; x <= 10; x++) {
+    res[x-1] = A * Math.Sqrt(B * x + D) - C * x;
+    if (res[x-1] > 0)  sump += res[x-1];
+    sum += res[x-1];
+}
+
+Console.WriteLine($"Сумма положительных значений: {sump}");
+Console.WriteLine($"Среднее значение: {(double) sum / 10}");
+Console.ReadKey();
+
---
+++ PracWork_3/task_5/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите количество чисел");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[] a = new int[n];
+int m = 0, index = 0;
+
+for (int i = 0; i < n; i++) {
+    Console.Write($"Введите число №{i + 1}: ");
+    a[i] = Convert.ToInt32(Console.ReadLine());
+    if (a[i] > m) {
+        m = a[i];
+        index = i;
+    }
+}
+
+int tmp = a[n - 1];
+a[n - 1] = a[index];
+a[index] = tmp;
+
+
+for (int i = 0; i < n; i++) Console.Write(a[i]);
+Console.ReadKey();
