66451c3c28deb242bba68c85cf69c69a3afc3e76
user <user@srv.faked>
2022-09-18 15:15:40 +0700

    18.09.2022

---
+++ PracWork_1/task_1/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Как вас зовут?");
+string name = Console.ReadLine();
+Console.WriteLine("Какой язык вы изучали?");
+string language = Console.ReadLine();
+Console.WriteLine($"Меня зовут {name}, я уже знаю {language}");
+Console.ReadKey();
---
+++ PracWork_1/task_2/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите X");
+int X = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите Y");
+int Y = Convert.ToInt32(Console.ReadLine());
+
+double result = ;
+Console.WriteLine(result);
